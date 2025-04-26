6d0b92d430d91fb02e17229f07d6eacfe732a09c
firoozmandy <maryam.firoozmandi@gmail.come>
2025-04-26 10:31:08 +0330

    add cart

--- marfa-next/src/app/page.tsx
+++ marfa-next/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import CardSlider from "./main/courses/card/card";
+
 // import { ButtonCourses, ButtonSelected } from "./main/courses/button/button";
 
 export default function Home() {
@@ -7,6 +9,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* <ButtonCourses>رهن و اجاره</ButtonCourses>
       <ButtonSelected> خانه</ButtonSelected> */}
+  <CardSlider/>
+  {/* <CardSlider/>
+  <CardSlider/>
+  <CardSlider/> */}
     </div>
   );
 }
